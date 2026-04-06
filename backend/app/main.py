c7753d16212f96a483dd293bb51524f76bc89935
Promise Emeziem Adiole <promzy007adiole@yahoo.com>
2026-04-06 15:45:03 +0200

    update main.py again

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://agentic-document-understanding-s88c.vercel.app",
+        "https://agentic-document-understanding-two.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
